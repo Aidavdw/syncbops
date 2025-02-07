21da71702ee33ae4160a5cfbe1e176280c4eff84
Aida (Laptop) <39270007+Aidavdw@users.noreply.github.com>
2025-02-07 14:08:09 +0100

    Add embedding of external cover art

--- src/ffmpeg_interface.rs
+++ src/ffmpeg_interface.rs
@@ -9,6 +9,9 @@ use std::{
 pub fn does_file_have_embedded_artwork(path: &Path) -> bool {
     let ffprobe = Command::new("ffprobe").arg(path).output().unwrap();
     let txt = String::from_utf8(ffprobe.stderr).unwrap();
+    // TODO: Instead, check if it has a video stream: If the title is different (the default for
+    // ffmpeg is "other") then it won't recognise it.
+    //TODO: Increase test coverage, to see if this works with .m4a, FLAC, etc.
     txt.contains("Cover")
 }
 
@@ -24,21 +27,40 @@ pub fn transcode_song(
         v_level < 9,
         "Presets for v-level compression only go up to 9. Be sure to sanitise input."
     );
+
+    let embed_external_artwork = include_album_art
+        && !does_file_have_embedded_artwork(source)
+        && external_album_art.is_some();
+
     let mut binding = Command::new("ffmpeg");
     binding
         .arg("-y") // Replace if it already exists
         .arg("-i")
-        .arg(source)
+        .arg(source);
+
+    if embed_external_artwork {
+        // safe to unwrap, already checked before.
+        binding.arg("-i").arg(external_album_art.unwrap());
+    };
+    binding
         .arg("-codec:a")
         .arg("libmp3lame")
         .arg("-q:a")
         .arg(v_level.to_string());
 
     // TODO: embed artwork if missing
-    if include_album_art {
-        ()
-        //if does_file_have_embedded_artwork(source) {}
-    } else {
+    if embed_external_artwork {
+        // It becomes `ffmpeg -i input.wav -i cover.jpg -codec:a libmp3lame -qscale:a 2 -metadata:s:v title="Cover" -metadata:s:v comment="Cover" -map 0:a -map 1:v output.mp3`
+        binding
+            .arg("-metadata:s:v")
+            .arg("title=\"Cover\"")
+            .arg("-metadata:s:v")
+            .arg("comments=\"Cover\"")
+            .arg("-map")
+            .arg("0:a")
+            .arg("-map")
+            .arg("1");
+    } else if !include_album_art {
         binding.arg("-vn");
     }
 
@@ -103,12 +125,44 @@ mod tests {
     }
 
     #[test]
+    fn transcode_no_embedded_album_art() -> Result<(), FfmpegError> {
+        let file_without_embedded_artwork: PathBuf =
+            "/home/aida/portable_music/Area 11/All The Lights In The Sky/1-02. Vectors.mp3".into();
+        let target: PathBuf = "/tmp/test_transcode_never_had_embedded_album_art.mp3".into();
+        transcode_song(&file_without_embedded_artwork, &target, 3, true, None)?;
+        // TODO: Throw an error if you include_album_art, but the final file is not able to embed
+        // album art.
+        assert!(!does_file_have_embedded_artwork(&target));
+
+        Ok(())
+    }
+
+    #[test]
     fn transcode_drop_album_art() -> Result<(), FfmpegError> {
         let file_with_embedded_artwork: PathBuf =
             "/home/aida/portable_music/Ado/狂言/04. FREEDOM.mp3".into();
         let target: PathBuf = "/tmp/test_transcode_drop_embedded_album_art.mp3".into();
         transcode_song(&file_with_embedded_artwork, &target, 3, false, None)?;
         assert!(!does_file_have_embedded_artwork(&target));
+
+        Ok(())
+    }
+
+    #[test]
+    fn transcode_embed_external_album_art() -> Result<(), FfmpegError> {
+        let file_without_embedded_artwork: PathBuf =
+            "/home/aida/portable_music/Area 11/All The Lights In The Sky/1-02. Vectors.mp3".into();
+        let external_artwork_file: PathBuf =
+            "/home/aida/portable_music/Area 11/All The Lights In The Sky/folder.jpg".into();
+        let target: PathBuf = "/tmp/test_transcode_newly_embedded_album_art.mp3".into();
+        transcode_song(
+            &file_without_embedded_artwork,
+            &target,
+            3,
+            true,
+            Some(&external_artwork_file),
+        )?;
+        assert!(does_file_have_embedded_artwork(&target));
 
         Ok(())
     }
